9d7fe23b823e84f09fbaca082cdc0ec33e9cc850
borntoplay189 <borntoplay189@gmail.com>
2016-03-19 18:53:47 +0000

    Primeira versao do site

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-/**
- * Created by LUCAS on 18/03/2016.
- */
 $( document ).ready(function() {
-    //Ativa o menu no mobile
     $(".button-collapse").sideNav();
+
+    $('.parallax').parallax();
     var telaAlt = $( window ).height();
     // Aplica o tamanho da foto de acordo com a tela
     $('.home').css('height',telaAlt);
@@ -13,3 +11,11 @@ $( document ).ready(function() {
     var contentHomeAlt = telaAlt - menuAlt;
     $('.contentHome').css('height',contentHomeAlt);*/
 });
+
+$(document).ready(function(){
+  				 $('.ancora').click(function(){
+     			 var alvo = $(this).attr('href').split('#').pop();
+      		$('html, body').animate({scrollTop: $('#'+alvo).offset().top }, 1000);
+      		return false;
+        });
+		});
